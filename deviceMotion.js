34ac5edbb78f07f11c36a6b504942665dbd6febb
Malkavian67 <samanta.duarte@outlook.fr>
2019-03-05 16:17:34 +0100

    Update deviceMotion.js

--- deviceMotion.js
+++ deviceMotion.js
@@ -65,4 +65,29 @@ function rotationHandler(rotation) {
 function intervalHandler(interval) {
   document.getElementById("moInterval").innerHTML = interval;
 }
+
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: [0,5,10,15,20,25,30],
+    datasets: [{ 
+        data: [86,114,106,106,107,111,133],
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fill: false
+      }, { 
+        data: [282,350,411,502,635,809,947],
+        label: "Asia",
+        borderColor: "#8e5ea2",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Motion (in seconds)'
+    }
+  }
+});
    
