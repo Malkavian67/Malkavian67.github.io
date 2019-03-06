ab60e4e2b4020950c401aa1d58d987a85d143992
Malkavian67 <samanta.duarte@outlook.fr>
2019-03-06 15:26:40 +0100

    Update Acceleration.js

--- Acceleration.js
+++ Acceleration.js
@@ -73,11 +73,11 @@ $(function()
     info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
     document.getElementById(targetId).innerHTML = info;
     
-    alert(info);
+    //alert(info);
 
     var length = Math.sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z );
 
-    alert(length);
+    //alert(length);
 
     Values.push(length);
     Label.push("");
