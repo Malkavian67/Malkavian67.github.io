7377513382f06b1acc40160e40b0efac55010ceb
Malkavian67 <samanta.duarte@outlook.fr>
2019-03-06 14:45:46 +0100

    Create Acceleration.js

---
+++ Acceleration.js
@@ -0,0 +1,76 @@
+$(function() {
+  $("#askButton").click( function()
+   {
+      if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
+        document.getElementById('moApi').innerHTML = 'Generic Sensor API';
+
+      let lastReadingTimestamp;
+
+      let duration=0;
+      let min=-1;
+      let max=-1;
+      let count=0;
+      let sum=0;
+      let avg = 0;
+
+      //acceleration
+      let accelerometer = new LinearAccelerationSensor();
+      accelerometer.addEventListener('reading', e => {
+        if (lastReadingTimestamp) {
+          intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
+        }
+        lastReadingTimestamp = accelerometer.timestamp
+        accelerationHandler(accelerometer, 'moAccel');
+      });
+      accelerometer.start();
+
+      } else if ('DeviceMotionEvent' in window) {
+        document.getElementById('moApi').innerHTML = 'Device Motion API';
+
+      var onDeviceMotion = function (eventData) {
+        accelerationHandler(eventData.acceleration, 'moAccel');
+        intervalHandler(eventData.interval);}
+  });
+});
+  
+
+window.addEventListener('devicemotion', onDeviceMotion, false);
+} else {
+  document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
+}
+
+var Values = [];
+var Label = [];
+
+function accelerationHandler(acceleration, targetId) {
+  var info, xyz = "[X, Y, Z]";
+
+  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
+  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
+  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
+  document.getElementById(targetId).innerHTML = info;
+  
+  
+  var length = Math.sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z );
+  
+  Values.push(length);
+  Label.push("");
+  
+  sum += length;
+  avg = sum / ++count;
+  
+  if(min == -1 || min > length){
+    min = length;
+  }
+  if(max == -1 || max < length){
+    max = length;
+  }  
+}
+
+function intervalHandler(interval) {
+  duration += interval
+  if(duration > 30000){
+    accelerometer.stop();    
+  }
+  document.getElementById("moInterval").innerHTML = interval;
+}
