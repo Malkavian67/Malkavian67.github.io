79d3cf76c55dc8d6d01df2de6a80a7bfd65e6fe0
Malkavian67 <samanta.duarte@outlook.fr>
2019-03-06 15:52:17 +0100

    Update Acceleration.js

--- Acceleration.js
+++ Acceleration.js
@@ -12,6 +12,9 @@ let sum=0;
 let avg = 0;
 
 let Values = [];
+let ValuesX = [];
+let ValuesY = [];
+let ValuesZ = [];
 let Label = [];
 
 $(function() 
@@ -26,6 +29,9 @@ $(function()
         avg = 0;
 
         Values = [];
+        ValuesX = [];
+        ValuesY = [];
+        ValuesZ = [];
         Label = [];
 
         if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) 
@@ -83,6 +89,10 @@ $(function()
     info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
     document.getElementById(targetId).innerHTML = info;
     
+    ValuesX.push(acceleration.x);
+    ValuesY.push(acceleration.y);
+    ValuesZ.push(acceleration.z);
+
     //alert(info);
 
     var length = Math.sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z );
@@ -138,6 +148,39 @@ $(function()
                         data: Values,
                         label: "Distance move",
                         borderColor: "#3e95cd",
+                        fill: false
+                    }
+                ]
+                },
+                options: {
+                title: {
+                    display: true,
+                    text: 'Motion (in seconds)'
+                }
+                }
+            });
+
+            new Chart(document.getElementById("line-chart2"), {
+                type: 'line',
+                data: {
+                labels: Label,
+                datasets: [
+                    { 
+                        data: ValuesX,
+                        label: "X",
+                        borderColor: "#a00000",
+                        fill: false
+                    },
+                    { 
+                        data: ValuesY,
+                        label: "Y",
+                        borderColor: "#00a000",
+                        fill: false
+                    },
+                    { 
+                        data: ValuesZ,
+                        label: "Z",
+                        borderColor: "#0000a0",
                         fill: false
                     }
                 ]
